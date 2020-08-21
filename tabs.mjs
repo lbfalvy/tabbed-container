8b66ec6f85d227a239658f8506ded32ca4f807d8
Lorinc Bethlenfalvy <lorinc11@gmail.com>
2020-08-21 15:43:24 +0200

    Rebuilt component
    Nodes are now moved in the tree when reordering.
    This makes everything much simpler, and iframe persistence is another
    component anyway.

--- tabs.mjs
+++ tabs.mjs
@@ -1,5 +1,15 @@
 import { hasPart, addPart, removePart } from "./parts.mjs";
 
+// Generate unique identifiers
+var next_uid = 0;
+/** @returns {Number} */
+var getUid = () => next_uid++;
+// Call this to set your own method
+/** @param {() => Number} gen */
+export function setUidGenerator(gen) {
+    getUid = gen;
+}
+
 const css = `
 #shadow-top {
     display: flex;
@@ -37,243 +47,147 @@ const css = `
 }
 `;
 
-/** 
- * @type {HTMLElement}
- * @private @global
- */
-var dragged;
 
-export default class extends HTMLElement
-{
-    constructor()
-    {
-        const self = super();
-        this.next_id = 0;
-        // === Build shadow tree ===
-        const shadow = this.attachShadow({ mode: "open" });
-        const stylesheet = document.createElement("style");
-        stylesheet.append(css);
-        const root = document.createElement("div");
-        root.id = "shadow-top";
-        const tabs = document.createElement("div");
-        tabs.id = "tabs";
-        tabs.setAttribute("part", "tab-bar");
-        tabs.ondrop = async ev => {
+export default class extends HTMLElement {
+    constructor() {
+        super();
+        // === Build shadow dom ===
+        const shadow = this.attachShadow({ mode: "open"});
+        this.stylesheet = document.createElement("style");
+        this.stylesheet.append(css);
+        this.tabs = document.createElement("div");
+        this.tabs.id = "tabs";
+        this.tabs.setAttribute("part", "tab-bar");
+        // Fetches the content and moves it under us in the right position.
+        this.tabs.ondrop = this.onTabDrop;
+        // Enable visual feedback for the drop zone
+        this.tabs.ondragenter = ev => addPart(this.tabs, "dragover");
+        this.tabs.ondragexit = ev => removePart(this.tabs, "dragover");
+        // Allow browser-specific feedback
+        this.tabs.ondragover = ev => {
             ev.preventDefault();
-            ev.stopPropagation();
-            removePart(tabs, "dragover");
-            if (!dragged) return;
-            const new_tab = await getDroppedTab(tabs);
-            new_tab.remove();
-            tabs.append(new_tab);
+            ev.dataTransfer.dropEffect = "move";
         }
-        tabs.ondragenter = ev => addPart(tabs, "dragover") || false;
-        tabs.ondragexit = ev => removePart(tabs, "dragover");
-        tabs.ondragover = () => false;
-        const contents = document.createElement("div");
-        contents.id = "contents";
-        contents.setAttribute("part", "contents");
-        root.append(tabs, contents)
-        shadow.append(stylesheet, root);
-        // === Register all children ===
-        for (let child of this.childNodes)
-        {
-            if (!(child instanceof HTMLElement)) continue;
-            this.registerContent(child);
-        }
-        // === Watch the tree ===
-        const tree_watcher = new MutationObserver(x => {
-            x.forEach(change => {
-                if (change.type == "childList") this.nodeListChanged(change);
-            })
-        });
-        tree_watcher.observe(this, { childList: true });
-        return self;
+        this.contents = document.createElement("slot");
+        shadow.append(this.stylesheet, this.tabs, this.contents);
+        
+        // === Observer ===
+        this.content_watcher = new MutationObserver(this.initWatcherAndTabs.bind(this));
+        this.initWatcherAndTabs();
     }
 
-    /** @param {MutationRecord} change */
-    nodeListChanged(change)
-    {
-        if (change.removedNodes.length > 0)
-        {
-            const contents = this.shadowRoot.getElementById("contents");
-            const shadow_top = this.shadowRoot.getElementById("shadow-top");
-            contents.childNodes.forEach(node => {
-                if (!(node instanceof HTMLElement)) return;
-                const uid = node.getAttribute("name");
-                const content_selector = `[slot="${uid}"]`;
-                if (!this.querySelector(content_selector))
-                {
-                    const related = shadow_top.getElementsByClassName(uid);
-                    while (0 < related.length) related[0].remove();
-                }
-                if (!this.hasFocus())
-                    this.selectTab();
+    /** @param {DragEvent} ev */
+    onTabGrab(ev) {
+        // Accept the event
+        ev.stopPropagation();
+        // Ensure it has an uid
+        var uid = this.getContentForTab(ev.currentTarget).getAttribute("data-uid");
+        // Send the uid with the drag event
+        ev.dataTransfer.setData("tabs/uid", uid);
+    };
+
+    /** @param {DragEvent} ev */
+    onTabDrop(ev) {
+        // Get the tab UID. Also ensure that the dragged thing is a tab.
+        const uid = ev.dataTransfer.getData("tabs/uid");
+        if (!uid) return;
+
+        // Accept the event and find special elements
+        ev.preventDefault();
+        /** @type {Element} */
+        const tabs = ev.currentTarget;
+        const container = tabs.getRootNode().host;
+        removePart(tabs, "dragover");
+
+        // Get the dragged content element
+        const content = document.querySelector(`[data-uid="${uid}"]`);
+        // If it was active, select the first one
+        if (content.hasAttribute("active")) {
+            setTimeout(() => content.parentElement.selectTab(0), 0);
+        }
+
+        // Get the index at which it is to be inserted
+        var i = 0;
+        while (i < tabs.childElementCount &&          // While there is still a child and
+               tabs.children[i] != ev.target &&       // neither is child the drop target
+               !tabs.children[i].contains(ev.target)) // nor does child contain it,
+            i++;                                      // increment the index
+        
+        // Insert the content
+        container.insertBefore(content, container.children[i]);
+        // Select this tab after the tab-bar had been updated
+        setTimeout(() => this.selectTab(i), 0);
+    }
+
+    selectTab(tab) {
+        if (typeof tab == "number") tab = this.tabs.children[tab];
+        if (!tab) return;
+        removePart(this.tabs.querySelector(`[part~="active"]`), "active");
+        this.querySelector(`[active]`)?.removeAttribute("active");
+        addPart(tab, "active");
+        this.getContentForTab(tab).setAttribute("active", "active");
+    }
+
+    closeTab(tab) {
+        this.getContentForTab(tab).remove();
+    }
+
+    getContentForTab(tab) {
+        const idx = Array.prototype.indexOf.call(this.tabs.children, tab);
+        return this.children[idx];
+    }
+
+    initWatcherAndTabs() {
+        // Completely redraw the tab bar and reconfigure the observer, to prevent watching moved tabs.
+        this.tabs.innerHTML = "";
+        this.content_watcher.disconnect();
+        this.content_watcher.observe(this, { childList: true});
+        for (const child of this.children) {
+            this.constructTab(child);
+            this.content_watcher.observe(child, { 
+                attributes: true, 
+                attributeFilter: ["data-title"] 
             });
         }
-        change.addedNodes.forEach(node => {
-            if (node instanceof HTMLElement) this.registerContent(node);
-        });
     }
 
-    /** @param {HTMLElement} content */
-    registerContent(content)
-    {
-        const uid = "uid-" + this.next_id++;
-        //#region Build html
+    constructTab(content) {
+        // Ensure the thing has a uid.
+        content.setAttribute("data-uid", getUid());
+        
+        // === Build HTML ===
         const tab = document.createElement("span");
-        tab.textContent = content.getAttribute("tab-name");
-        tab.classList.add(uid);
-        tab.setAttribute("data-uid", uid);
+        tab.textContent = content.getAttribute("data-title");
         tab.setAttribute("part", "tab");
+        if (content.getAttribute("active")) addPart(tab, "active");
         tab.setAttribute("draggable", "true");
         this.shadowRoot.getElementById("tabs").append(tab);
-        const label = document.createElement("span");
-        tab.append(label);
         tab.onclick = ev => {
             ev.stopPropagation();
-            this.selectTab(uid);
+            const host = ev.currentTarget.getRootNode().host;
+            host.selectTab(tab);
         };
-        //#endregion
-        //#region Drag events
-        tab.ondragstart = ev => {
-            ev.stopPropagation();
-            dragged = tab;
-        };
-        tab.ondrop = async ev => {
-            ev.preventDefault();
-            ev.stopPropagation();
-            removePart(tab, "dragover");
-            if (tab == dragged) return;
-            const new_tab = await getDroppedTab(tab);
-            new_tab.remove();
-            tab.insertAdjacentElement("beforebegin", new_tab);
-        };
+        
+        // === Drag events ===
+        tab.ondragstart = this.onTabGrab.bind(this);
+        // Catch these so they don't bubble to the tab-bar.
         tab.ondragenter = ev => {
             ev.stopPropagation();
-            addPart(tab, "dragover");
-            return false;
+            addPart(ev.currentTarget, "dragover");
         };
         tab.ondragexit = ev => {
             ev.stopPropagation();
-            removePart(tab, "dragover");
+            removePart(ev.currentTarget, "dragover");
         };
-        tab.ondragover = () => false;
-        //#endregion
-        //#region Close button
+        
+        // === Close button ===
         const close = document.createElement("i");
         close.setAttribute("part", "close");
         tab.append(close);
         close.onclick = ev => {
             ev.stopPropagation();
-            content.remove()
+            const host = ev.currentTarget.getRootNode().host
+            host.closeTab(ev.currentTarget);
         };
-        //#endregion
-        // === Watch for name changes ===
-        const name_watcher = new MutationObserver(x => {
-            label.textContent = content.getAttribute("tab-name");
-        });
-        name_watcher.observe(content, {
-            attributes: true, 
-            attributeFilter: ["tab-name"]
-        });
-        // === Create and connect slot ===
-        content.setAttribute("slot", uid);
-        const slot = document.createElement("slot");
-        slot.name = uid;
-        slot.classList.add(uid);
-        this.shadowRoot.getElementById("contents").append(slot);
-        // === Select newly added tab if there wasn't a selected one already ===
-        if (!this.hasFocus())
-            this.selectTab(uid);
     }
-
-    /** @returns {Boolean} */
-    hasFocus()
-    {
-        const shadow_top = this.shadowRoot.getElementById("shadow-top");
-        return shadow_top.getElementsByClassName("active").length > 0;
-    }
-
-    selectTab(uid = "")
-    {
-        if (!uid) 
-        {
-            const root = this.shadowRoot.getElementById("shadow-top");
-            const first_slot = root.getElementsByTagName("slot")[0];
-            // Select the first tab
-            if (!first_slot) return; // If the container isn't empty
-            uid = first_slot.name;
-        }
-        const root = this.shadowRoot.getElementById("shadow-top");
-        const active = root.getElementsByClassName("active");
-        while (0 < active.length) 
-        {
-            if (hasPart(active[0], "tab")) 
-            {
-                removePart(active[0], "active");
-                const close = active[0].getElementsByTagName("i")[0];
-                removePart(close, "active");
-            }
-            active[0].classList.remove("active");
-        }
-        const related = root.getElementsByClassName(uid);
-        for (let i = 0; i < related.length; i++) 
-        {
-            if (hasPart(related[i], "tab")) 
-            {
-                addPart(related[i], "active");
-                const close = related[i].getElementsByTagName("i")[0];
-                addPart(close, "active");
-            }
-            related[i].classList.add("active");
-        }
-    }
-}
-
-/**
- * Handle the case where the user moved the tab to a different container
- * @param {HTMLElement} drop_target 
- * @returns {Promise<HTMLElement>}
- * @async
- */
-function getDroppedTab(drop_target)
-{
-    return new Promise(resolve => {
-        // If it was local
-        if (dragged.getRootNode() == drop_target.getRootNode()) 
-        {
-            // Just reset dragged and return it
-            const target = dragged;
-            dragged = null;
-            resolve(target);
-        }
-        // If it was across containers
-        else
-        {
-            // Get the content
-            /** @type {HTMLElement} */
-            const original_container = dragged.getRootNode().host;
-            const original_uid = dragged.getAttribute("data-uid");
-            const content_selector = `[slot="${original_uid}"]`;
-            const content = original_container.querySelector(content_selector);
-            // Move the content
-            content.remove();
-            drop_target.getRootNode().host.append(content);
-            // Wait for the new grid to handle the move event
-            setTimeout(() => {
-                // Forget the old tab
-                dragged.remove();
-                dragged = null;
-                // Get the new tab
-                const new_uid = content.getAttribute("slot");
-                /** @type {ShadowRoot} */
-                const new_shadowroot = drop_target.getRootNode();
-                const tab_selector = `[data-uid="${new_uid}"]`;
-                const new_tab = new_shadowroot.querySelector(tab_selector);
-                // Resolve with the new tab
-                resolve(new_tab);
-            }, 10);
-        }
-    });
 }
